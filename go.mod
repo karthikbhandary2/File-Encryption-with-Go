261953fd60a3b4bdb3e83c5f3a6243f308e4366d
karthikbhandary2 <karthikbhandary2@gmail.com>
2025-10-24 07:07:53 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/karthikbhandary2/file-encryption
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		printHelp()
+		os.Exit(1)
+	}
+	function := os.Args[1]
+	switch function {
+	case "help":
+		printHelp()
+	case "encrypt":
+		encryptHandle()
+	case "decrypt":
+		dencryptHandle()
+	default:
+		fmt.Println("Run encrypt to encrypt a file, and decrypt to decrypt a file.")
+		os.Exit(1)
+	}
+}
+
+func printHelp() {
+	fmt.Println("file encryption")
+	fmt.Println("Simple file encrypter for your day-to-day needs.")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("\t go run . encrypt /path/to/your/file")
+	fmt.Println("")
+	fmt.Println("Commands:")
+	fmt.Println("")
+	fmt.Println("\t encrypt\tEncrypts a file given a password")
+	fmt.Println("\t decrypt\tTries to decrypt a file using a password")
+	fmt.Println("\t help \t\tDisplays help text")
+	fmt.Println("")
+
+}
+
+func encryptHandle() {
+
+}
+
+func dencryptHandle() {
+
+}
+
+func getPassword() {
+
+}
+
+func validatePassword() {
+
+}
+
+func validateFile() {
+
+}
